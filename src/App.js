4be42ee73a1355fa9ca150fc1e244195e4272d24
Scarlet Alkeyo <scarletalkeyo@gmail.com>
2024-06-17 19:12:54 +0300

    done landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Intro from "./Landingpage";
 import Navb from "./Navbar";
 
 function Nav(){
   return(
     <div>
+      
       <Navb/>
+      <Intro/>
     </div>
   );
 };
---
+++ src/Landingpage/index.js
@@ -0,0 +1,19 @@
+import'./index.css';
+
+const Intro =()=>{
+    return(
+        <div className='intro'>
+            
+            <h1 className='content'>BRINGING ACCESS TO</h1>
+            <h1>MOBILE MONEY CLOSER</h1>
+            <h1>TO FARMERS</h1>
+             <button id='button'>Learn more</button>
+
+
+        
+            
+        </div>
+    );
+};
+
+export default  Intro;
