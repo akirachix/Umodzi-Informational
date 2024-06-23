5a6b745be0778a1ad7a9681b3731d1c663daa23c
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-06-23 18:38:35 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Intro from "./Landingpage";
-import Navb from "./Navbar";
 import Products from "./Product";
 
 function Nav(){
   return(
     <div>
       
-      <Navb/>
-      <Intro/>
       <Products/>
     </div>
   );
--- src/Product/index.js
+++ src/Product/index.js
@@ -3,18 +3,19 @@ import ganza from './ganza.png';
 import google from './ganzagp.png';
 const Products = () => {
   return (
+    
     <div className="product">
       <div className="text">
-        
+        <h3>Product</h3>
         <p className="product-text">
-          GanzaPay is gamified app for mobile money apps.<br />With GanzaPay you can send money,<br /> withdraw money, check balance and call customer care<br /> without the need to read or write.
+          GanzaPay is gamified app for mobile money apps.<br/>With GanzaPay you can send money, withdraw money, <br/>check balance and call customer care without the need to read or write.
         </p>
         <h2>Download the App On</h2>
-        <img className="google-play-img" src={google} alt="" />
+        <img id="google-play-img" src={google} alt="" />
       </div>
-      <div id="image">
-        <img src={ganza} alt="" />
-      </div>
+      <div>
+        <img src={ganza} alt=""  id='image'/>
+        </div>
     </div>
   );
 };
