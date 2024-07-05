1abc874b27b9a3e5b7ecdc6149edd8d53d0c1fae
Wangesha-Agnes <wangeshaagnes@gmail.com>
2024-07-05 17:42:58 +0300

    changes added

--- src/Product/index.js
+++ src/Product/index.js
@@ -1,20 +1,21 @@
 import './index.css';
-import ganza from './ganza.png';
-import google from './ganzagp.png';
+
+
 const Products = () => {
   return (
     
     <div className="product">
-      <div className="text">
+      <div className="body">
         <h3>Product</h3>
         <p className="product-text">
         GanzaPay presents an innovative approach to financial transactions through mobile applications, leveraging gamification elements to enhance user experience and accessibility. This app combines the convenience of digital payments with engaging game-like features, aiming to make financial operations more interactive and intuitive.
         </p>
         <h2 className='download'>Download the App On</h2>
-        <img id="google-play-img" src={google} alt="" />
+        <img src='/images/ganzagp.png' alt='gaza' id='google-play-img'/>
       </div>
       <div>
-        <img src={ganza} alt=""  id='image'/>
+     <img src='/images/ganza.png' alt='' id='image'/>
+
         </div>
     </div>
   );
