1eb1a0d600746f9979c9c9cf43b28fdc4d4065eb
student <faithnyamburawamui@gmail.com>
2024-07-10 08:07:49 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 
 import Contact from "./Contact";
 
+
 function App(){
   return(
     <div>
-      <Contact/
-import Footer from "./Footer";
+      <Contact/>
+      </div>
+      )
+      }
 
-function Foot(){
-  return(
-    <div>
-      <Footer/>
+    
+    export default App;
+    
 
-    </div>
-  );
-};
 
-export default App;
 
-export default Foot;
